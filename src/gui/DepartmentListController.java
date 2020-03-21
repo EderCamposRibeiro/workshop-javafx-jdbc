2ab0c7951884fb93499d6622394ec3600ef7e2c5
EderCamposRibeiro <45081914+EderCamposRibeiro@users.noreply.github.com>
2020-03-21 10:37:18 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -17,7 +17,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListControler implements Initializable{
+public class DepartmentListController implements Initializable{
 	
 	private DepartmentService service;
 
